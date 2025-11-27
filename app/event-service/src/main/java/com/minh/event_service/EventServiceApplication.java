822777542e6dc0d8bfebb3d0060ab269a0c3ac3d
tts_minhmv <tts_minhmv@viettelsoftware.com>
2025-11-27 23:22:19 +0700

    feat: Add game features code base.

---
+++ app/event-service/src/main/java/com/minh/event_service/EventServiceApplication.java
@@ -0,0 +1,25 @@
+package com.minh.event_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableJpaRepositories(basePackages = "com.minh.event_service.repository")
+@EntityScan(value = "com.minh.event_service.entity")
+@ComponentScan(basePackages = {"com.minh.event_service.*", "com.minh.common"})
+@EnableMethodSecurity
+public class EventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServiceApplication.class, args);
+	}
+
+}
